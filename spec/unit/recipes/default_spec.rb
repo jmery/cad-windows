daa3395b651d59a071e1d3cd47de22388308fa62
Chef Sales Engineering <saleseng@chef.io>
2016-02-15 06:17:17 +0000

    adding chefspec tests

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -3,18 +3,31 @@
 # Spec:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-
 require 'spec_helper'
+
+# RSpec.configure do |config|
+#   config.platform = 'windows'
+#   config.version  = '2012R2'
+# end
 
 describe 'sitedbaas-windows::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'windows', version: '2012R2')
       runner.converge(described_recipe)
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'runs a powershell_script to install IIS server' do
+      expect(chef_run).to run_powershell_script('Install IIS')
+    end
+
+    it 'creates a template c:\inetpub\wwwroot\Default.htm' do
+      expect(chef_run).to create_template('c:\inetpub\wwwroot\Default.htm')
+    end
+
+    it 'starts and enables w3svc' do
+      expect(chef_run).to start_service('w3svc')
+      expect(chef_run).to enable_service('w3svc')
     end
   end
 end
